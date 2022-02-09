fde81e645b422fc3066104e51ba1a638c332a127
Nguyen Dinh Tuan Anh <ndtatuananh@gmail.com>
2022-02-09 16:20:56 +0700

    display profile, login bug => fetch bug, need fixing

--- src/pages/user/[user_name]/index.js
+++ src/pages/user/[user_name]/index.js
@@ -1,18 +1,25 @@
 import HeadTitle from "common/components/headtitle/HeadTitle";
 import { useEffect, useState } from "react";
 import { getUserProfile } from "modules/users/api/users.api";
-import { Container, Grid, Typography } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
 
 export default function ViewProfile() {
+  const classes = useStyle();
   const gridItemProperty = {
     property: {
       item: true,
-      xs: 2,
+      xs: 4,
+      sm: 5,
+      md: 4,
+      lg: 3,
     },
-
     value: {
       item: true,
-      xs: 10,
+      xs: 8,
+      sm: 7,
+      md: 8,
+      lg: 9,
     },
   };
 
@@ -26,57 +33,61 @@ export default function ViewProfile() {
   return (
     <>
       <HeadTitle page="profile" />
-
-      <h1>Profile</h1>
       <Grid container>
-        {/* Username */}
-        <Grid {...gridItemProperty.property}>
-          <h3>Username</h3>
+        <Grid item xs={12} sm={4} className={classes.profileTitle}>
+          <h1>Profile</h1>
         </Grid>
-        <Grid {...gridItemProperty.value}>
-          <p>{profile.user_name}</p>
+        <Grid item xs={12} sm={8} container className={classes.infoContainer}>
+          <Grid {...gridItemProperty.property}>
+            <h3>Username</h3>
+          </Grid>
+          <Grid {...gridItemProperty.value}>
+            <p>{profile.user_name}</p>
+          </Grid>
+          <Grid {...gridItemProperty.property}>
+            <h3>Email</h3>
+          </Grid>
+          <Grid {...gridItemProperty.value}>
+            <p>{profile.email}</p>
+          </Grid>
+          <Grid {...gridItemProperty.property}>
+            <h3>Full name</h3>
+          </Grid>
+          <Grid {...gridItemProperty.value}>
+            <p>{profile.full_name}</p>
+          </Grid>
+          <Grid {...gridItemProperty.property}>
+            <h3>Role</h3>
+          </Grid>
+          <Grid {...gridItemProperty.value}>
+            <p>{profile.role}</p>
+          </Grid>
+          <Grid {...gridItemProperty.property}>
+            <h3>Gender</h3>
+          </Grid>
+          <Grid {...gridItemProperty.value}>
+            <p>{profile.gender}</p>
+          </Grid>
         </Grid>
-
-        {/* Email */}
-        <Grid {...gridItemProperty.property}>
-          <h3>Email</h3>
-        </Grid>
-        <Grid {...gridItemProperty.value}>
-          <p>{profile.email}</p>
-        </Grid>
-
-        {/* Role */}
-        <Grid {...gridItemProperty.property}>
-          <h3>Role</h3>
-        </Grid>
-        <Grid {...gridItemProperty.value}>
-          <p>{profile.role}</p>
-        </Grid>
-
-        {/* Full name */}
-        <Grid {...gridItemProperty.property}>
-          <h3>Full name</h3>
-        </Grid>
-        <Grid {...gridItemProperty.value}>
-          <p>{profile.full_name}</p>
-        </Grid>
-
-        {/* Gender */}
-        <Grid {...gridItemProperty.property}>
-          <h3>Gender</h3>
-        </Grid>
-        <Grid {...gridItemProperty.value}>
-          <p>{profile.gender}</p>
-        </Grid>
-
-        {/* Date of Birth */}
-        {/* <Grid {...gridItemProperty.property}>
-          <h3>Date of birth</h3>
-        </Grid>
-        <Grid {...gridItemProperty.value}>
-          <p>{profile.day_of_birth}</p>
-        </Grid> */}
       </Grid>
     </>
   );
 }
+
+const useStyle = makeStyles((theme) => ({
+  profileTitle: {
+    textAlign: "center",
+  },
+
+  infoContainer: {
+    borderLeft: `4px solid ${theme.palette.primary.light}`,
+    paddingLeft: "30px",
+  },
+
+  [theme.breakpoints.down("xs")]: {
+    infoContainer: {
+      borderLeft: "none",
+      paddingLeft: "none",
+    },
+  },
+}));
