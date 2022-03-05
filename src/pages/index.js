a7649e6e58e809e7ebd5455018e1f57b05c2ed4b
hoanglong040800 <hoanglong040800@gmail.com>
2022-03-05 10:44:54 +0700

    Integrate filter API

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,7 @@ export default function Home() {
 	const [params, setParams] = useState({
 		cursor: null,
 		limit: BOOK_LIMIT,
+		// TODO LOW: FILTER LATEST DATE
 	})
 	const [hasMore, setHasMore] = useState(true)
 
