8a5b3dc2109161f307277e19525950925f71dd8a
hoanglong040800 <hoanglong040800@gmail.com>
2022-03-06 11:57:52 +0700

    feature Genre button forward filter page

--- src/modules/books/components/detail/GenreList.js
+++ src/modules/books/components/detail/GenreList.js
@@ -1,29 +1,50 @@
 import { Button } from '@material-ui/core'
+import { useRouter } from 'next/router'
 import React from 'react'
 
 export default function GenreList({ genres }) {
+	const router = useRouter()
+
+	function onClickButton(id) {
+		router.push({
+			pathname: '/books/filter',
+			query: { genre: id },
+		})
+	}
+
 	return (
-		<div
-			style={{
-				display: 'flex',
-				flexWrap: 'wrap',
-				gap: '10px',
-				marginTop: '20px',
-			}}
-		>
+		<div style={styles.container}>
 			{genres.map((genre, index) => (
 				<Button
 					key={genre.id}
-					variant="outlined"
-					color="primary"
-					style={{
-						borderRadius: '50px',
-						fontWeight: 'normal',
-					}}
+					onClick={() => onClickButton(genre.id)}
+					{...props.btn}
 				>
 					{genre.name_en.toUpperCase()}
 				</Button>
 			))}
 		</div>
 	)
+}
+
+const styles = {
+	btn: {
+		borderRadius: '50px',
+		fontWeight: 'normal',
+	},
+
+	container: {
+		display: 'flex',
+		flexWrap: 'wrap',
+		gap: '10px',
+		marginTop: '20px',
+	},
+}
+
+const props = {
+	btn: {
+		variant: 'outlined',
+		color: 'primary',
+		style: styles.btn,
+	},
 }
