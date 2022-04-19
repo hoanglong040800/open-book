4296e176332f685c4c0a238b700801f7480b9eed
hoanglong040800 <hoanglong040800@gmail.com>
2022-04-19 10:14:00 +0700

    include isAuth to auth guard

--- src/modules/auth/components/AuthGuard.js
+++ src/modules/auth/components/AuthGuard.js
@@ -3,35 +3,43 @@ import { signIn, useSession } from 'next-auth/client'
 import Loading from 'common/components/loading/Loading'
 import { useRouter } from 'next/router'
 
-export default function AuthGuard({ children, allowedRole }) {
+export default function AuthGuard({ children, isAuth, allowedRole }) {
 	const [session, loading] = useSession()
 	const router = useRouter()
 	const isSignin = !!session?.user
 
-	useEffect(() => {
-		if (loading) return // Do nothing while loading
-		if (!isSignin) signIn() // If not authenticated, force log in
-	}, [isSignin, loading])
+	// authorize user & role
+	if (isAuth) {
+		useEffect(() => {
+			if (loading) return // Do nothing while loading
+			if (!isSignin) signIn() // If not authenticated, force log in
+		}, [isSignin, loading])
 
-	if (isSignin) {
-		// role check
-		if (allowedRole) {
-			if (allowedRole === session.user.role) {
+		if (isSignin) {
+			// role check
+			if (allowedRole) {
+				if (allowedRole === session.user.role) {
+					return children
+				}
+				// prevent user to access page
+				else {
+					router.push('/404')
+				}
+			}
+
+			// no role check mean no need
+			else {
 				return children
 			}
-			// prevent user to access page
-			else {
-				router.push('/404')
-			}
 		}
 
-		// no role check mean no need
-		else {
-			return children
-		}
+		// Session is being fetched, or no user.
+		// If no user, useEffect() will redirect.
+		return <Loading />
 	}
 
-	// Session is being fetched, or no user.
-	// If no user, useEffect() will redirect.
-	return <Loading />
+	// no auth needed
+	else {
+		return children
+	}
 }
